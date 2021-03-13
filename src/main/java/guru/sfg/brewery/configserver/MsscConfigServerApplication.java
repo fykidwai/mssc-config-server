f025f176ed7346af9ad8e45936ef1921d75ef2e8
Faiz Kidwai <f.y.kidwai@gmail.com>
2021-03-13 10:23:12 +0530

    Configuring for GitHub Example Repo

--- src/main/java/guru/sfg/brewery/configserver/MsscConfigServerApplication.java
+++ src/main/java/guru/sfg/brewery/configserver/MsscConfigServerApplication.java
@@ -2,12 +2,14 @@ package guru.sfg.brewery.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class MsscConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsscConfigServerApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(MsscConfigServerApplication.class, args);
+    }
 
 }
